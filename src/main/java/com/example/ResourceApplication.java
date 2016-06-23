e22afac7c6748aa73eac79ca288e4f128e4bde4a
Hanxian Lin <saitoplam@hotmail.com>
2016-06-23 22:55:06 +0800

    allow CORS request form spring-boot-sample server

--- src/main/java/com/example/ResourceApplication.java
+++ src/main/java/com/example/ResourceApplication.java
@@ -5,17 +5,32 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsUtils;
 
 
 @SpringBootApplication
 @RestController
-class ResourceApplication {
+class ResourceApplication extends WebSecurityConfigurerAdapter  {
+
+	@Bean
+	HeaderHttpSessionStrategy sessionStrategy() {
+		return new HeaderHttpSessionStrategy();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().requestMatchers(CorsUtils::isCorsRequest).permitAll().anyRequest().authenticated();
+	}
+
 
 	@RequestMapping("/")
-	@CrossOrigin(origins="*", maxAge=3600)
+	@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders={"x-auth-token", "x-requested-with"})
 	public Message home() {
 		return new Message("Hello World");
 	}
